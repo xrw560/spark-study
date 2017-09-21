5412290cc009d1584cad509037ab19c3c41955dc
周宁 <zhouning@yzhtech.com>
2017-09-21 14:04:02 +0800

    no message

--- spark-study-scala/src/cn/spark/study/core/LocalFile.scala
+++ spark-study-scala/src/cn/spark/study/core/LocalFile.scala
@@ -4,20 +4,20 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
 /**
- * @author Administrator
- */
+  * @author Administrator
+  */
 object LocalFile {
-  
+
   def main(args: Array[String]) {
     val conf = new SparkConf()
-        .setAppName("LocalFile") 
-        .setMaster("local");  
+      .setAppName("LocalFile")
+      .setMaster("local")
     val sc = new SparkContext(conf)
-    
-    val lines = sc.textFile("C://Users//Administrator//Desktop//spark.txt", 1);
-    val count = lines.map { line => line.length() }.reduce(_ + _)  
-    
-    println("file's count is " + count)  
+
+    val lines = sc.textFile("data/spark.txt", 1)
+    val count = lines.map { line => line.length() }.reduce(_ + _)
+
+    println("file's count is " + count)
   }
-  
+
 }
