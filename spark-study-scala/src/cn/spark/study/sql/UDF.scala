8444eda7480a4d7592785528edcdd5ee1cabb278
MicroWin10-1512\Administrator <ncutits@163.com>
2017-09-26 22:59:47 +0800

    UDF自定义函数实战

--- spark-study-scala/src/cn/spark/study/sql/UDF.scala
+++ spark-study-scala/src/cn/spark/study/sql/UDF.scala
@@ -9,36 +9,38 @@ import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 
 /**
- * @author Administrator
- */
+  * UDF自定义函数实战
+  *
+  * @author Administrator
+  */
 object UDF {
-  
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-        .setMaster("local") 
-        .setAppName("UDF")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-  
-    // 构造模拟数据
-    val names = Array("Leo", "Marry", "Jack", "Tom") 
-    val namesRDD = sc.parallelize(names, 5) 
-    val namesRowRDD = namesRDD.map { name => Row(name) }
-    val structType = StructType(Array(StructField("name", StringType, true)))  
-    val namesDF = sqlContext.createDataFrame(namesRowRDD, structType) 
-    
-    // 注册一张names表
-    namesDF.registerTempTable("names")  
-    
-    // 定义和注册自定义函数
-    // 定义函数：自己写匿名函数
-    // 注册函数：SQLContext.udf.register()
-    sqlContext.udf.register("strLen", (str: String) => str.length()) 
-  
-    // 使用自定义函数
-    sqlContext.sql("select name,strLen(name) from names")
-        .collect()
-        .foreach(println)  
-  }
-  
+
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf()
+                .setMaster("local")
+                .setAppName("UDF")
+        val sc = new SparkContext(conf)
+        val sqlContext = new SQLContext(sc)
+
+        // 构造模拟数据
+        val names = Array("Leo", "Marry", "Jack", "Tom")
+        val namesRDD = sc.parallelize(names, 5)
+        val namesRowRDD = namesRDD.map { name => Row(name) }
+        val structType = StructType(Array(StructField("name", StringType, true)))
+        val namesDF = sqlContext.createDataFrame(namesRowRDD, structType)
+
+        // 注册一张names表
+        namesDF.registerTempTable("names")
+
+        // 定义和注册自定义函数
+        // 定义函数：自己写匿名函数
+        // 注册函数：SQLContext.udf.register()
+        sqlContext.udf.register("strLen", (str: String) => str.length())
+
+        // 使用自定义函数
+        sqlContext.sql("select name,strLen(name) from names")
+                .collect()
+                .foreach(println)
+    }
+
 }
