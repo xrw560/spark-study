5a1206d65cbda4d26d4725f2816f5ef04b9337b7
周宁 <zhouning@yzhtech.com>
2017-09-29 14:59:44 +0800

    实时wordcount程序开发

--- spark-study-scala/src/cn/spark/study/streaming/WordCount.scala
+++ spark-study-scala/src/cn/spark/study/streaming/WordCount.scala
@@ -5,26 +5,29 @@ import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.StreamingContext
 
 /**
- * @author Administrator
- */
+  * @author Administrator
+  */
 object WordCount {
-  
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-        .setMaster("local[2]")  
-        .setAppName("WordCount")
-        
-    val ssc = new StreamingContext(conf, Seconds(5))
-    
-    val lines = ssc.socketTextStream("spark1", 9999)
-    val words = lines.flatMap { _.split(" ") }   
-    val pairs = words.map { word => (word, 1) }  
-    val wordCounts = pairs.reduceByKey(_ + _)  
-    
-    wordCounts.print()  
-    
-    ssc.start()
-    ssc.awaitTermination()
-  }
-  
+
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf()
+                .setMaster("local[2]")
+                .setAppName("WordCount")
+
+        // Scala中，创建的是StreamingContext
+        val ssc = new StreamingContext(conf, Seconds(5))
+
+        val lines = ssc.socketTextStream("spark1", 9999)
+        val words = lines.flatMap {
+            _.split(" ")
+        }
+        val pairs = words.map { word => (word, 1) }
+        val wordCounts = pairs.reduceByKey(_ + _)
+
+        wordCounts.print()
+
+        ssc.start()
+        ssc.awaitTermination()
+    }
+
 }
