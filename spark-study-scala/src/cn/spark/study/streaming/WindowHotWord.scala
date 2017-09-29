8b16e8fd319b44904a83c8f9a4a4b97a55fd2fef
周宁 <zhouning@yzhtech.com>
2017-09-29 17:59:26 +0800

    window滑动窗口

--- spark-study-scala/src/cn/spark/study/streaming/WindowHotWord.scala
+++ spark-study-scala/src/cn/spark/study/streaming/WindowHotWord.scala
@@ -5,41 +5,47 @@ import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.Seconds
 
 /**
- * @author Administrator
- */
+  * @author Administrator
+  */
 object WindowHotWord {
-  
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-        .setMaster("local[2]")  
-        .setAppName("WindowHotWord")
-    val ssc = new StreamingContext(conf, Seconds(1))
-    
-    val searchLogsDStream = ssc.socketTextStream("spark1", 9999)  
-    val searchWordsDStream = searchLogsDStream.map { _.split(" ")(1) }  
-    val searchWordPairsDStream = searchWordsDStream.map { searchWord => (searchWord, 1) }  
-    val searchWordCountsDSteram = searchWordPairsDStream.reduceByKeyAndWindow(
-        (v1: Int, v2: Int) => v1 + v2, 
-        Seconds(60), 
-        Seconds(10))  
-        
-    val finalDStream = searchWordCountsDSteram.transform(searchWordCountsRDD => {
-      val countSearchWordsRDD = searchWordCountsRDD.map(tuple => (tuple._2, tuple._1))  
-      val sortedCountSearchWordsRDD = countSearchWordsRDD.sortByKey(false)  
-      val sortedSearchWordCountsRDD = sortedCountSearchWordsRDD.map(tuple => (tuple._1, tuple._2))
-      
-      val top3SearchWordCounts = sortedSearchWordCountsRDD.take(3)
-      for(tuple <- top3SearchWordCounts) {
-        println(tuple)
-      }
-      
-      searchWordCountsRDD
-    })
-    
-    finalDStream.print()
-    
-    ssc.start()
-    ssc.awaitTermination()
-  }
-  
+
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf()
+                .setMaster("local[2]")
+                .setAppName("WindowHotWord")
+        val ssc = new StreamingContext(conf, Seconds(1))
+
+        val searchLogsDStream = ssc.socketTextStream("spark1", 9999)
+        val searchWordsDStream = searchLogsDStream.map {
+            _.split(" ")(1)
+        }
+        val searchWordPairsDStream = searchWordsDStream.map { searchWord => (searchWord, 1) }
+        val searchWordCountsDStream = searchWordPairsDStream.reduceByKeyAndWindow(
+            (v1: Int, v2: Int) => v1 + v2,
+            Seconds(60),
+            Seconds(10))
+
+        val finalDStream = searchWordCountsDStream.transform(searchWordCountsRDD => {
+            // 反转
+            val countSearchWordsRDD = searchWordCountsRDD.map(tuple => (tuple._2, tuple._1))
+            // 排序
+            val sortedCountSearchWordsRDD = countSearchWordsRDD.sortByKey(false)
+            // 反转回来
+            val sortedSearchWordCountsRDD = sortedCountSearchWordsRDD.map(tuple => (tuple._1, tuple._2))
+
+            val top3SearchWordCounts = sortedSearchWordCountsRDD.take(3)
+            for (tuple <- top3SearchWordCounts) {
+                println(tuple)
+            }
+
+            searchWordCountsRDD
+        })
+
+        // 这个无关紧要，只是为了触发job的执行，所以必须有output操作
+        finalDStream.print()
+
+        ssc.start()
+        ssc.awaitTermination()
+    }
+
 }
