501651986a7e8f825b6a84f7648fc1422b76d559
MicroWin10-1512\Administrator <ncutits@163.com>
2017-09-30 00:17:10 +0800

    no message

--- spark-study-java/src/main/java/cn/spark/study/streaming/PersistWordCount.java
+++ spark-study-java/src/main/java/cn/spark/study/streaming/PersistWordCount.java
@@ -25,113 +25,113 @@ import scala.Tuple2;
 
 /**
  * 基于持久化机制的实时wordcount程序
- * @author Administrator
  *
+ * @author Administrator
  */
 public class PersistWordCount {
 
-	public static void main(String[] args) {
-		SparkConf conf = new SparkConf()
-				.setMaster("local[2]")
-				.setAppName("PersistWordCount");  
-		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+                .setMaster("local[2]")
+                .setAppName("PersistWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
-		jssc.checkpoint("hdfs://spark1:9000/wordcount_checkpoint");  
-		
-		JavaReceiverInputDStream<String> lines = jssc.socketTextStream("spark1", 9999);
-		
-		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+        jssc.checkpoint("hdfs://spark1:9000/wordcount_checkpoint");
 
-			private static final long serialVersionUID = 1L;
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("spark1", 9999);
 
-			@Override
-			public Iterable<String> call(String line) throws Exception {
-				return Arrays.asList(line.split(" "));  
-			}
-			
-		});
-		
-		JavaPairDStream<String, Integer> pairs = words.mapToPair(
-				
-				new PairFunction<String, String, Integer>() {
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
 
-					private static final long serialVersionUID = 1L;
+            private static final long serialVersionUID = 1L;
 
-					@Override
-					public Tuple2<String, Integer> call(String word)
-							throws Exception {
-						return new Tuple2<String, Integer>(word, 1);
-					}
-					
-				});
+            @Override
+            public Iterable<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" "));
+            }
 
-		JavaPairDStream<String, Integer> wordCounts = pairs.updateStateByKey(
-				
-				new Function2<List<Integer>, Optional<Integer>, Optional<Integer>>() {
+        });
 
-					private static final long serialVersionUID = 1L;
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(
 
-					@Override
-					public Optional<Integer> call(List<Integer> values,
-							Optional<Integer> state) throws Exception {
-						Integer newValue = 0;
-						
-						if(state.isPresent()) {
-							newValue = state.get();
-						}
+                new PairFunction<String, String, Integer>() {
 
-						for(Integer value : values) {
-							newValue += value;
-						}
-						
-						return Optional.of(newValue);  
-					}
-					
-				});
-		
-		// 每次得到当前所有单词的统计次数之后，将其写入mysql存储，进行持久化，以便于后续的J2EE应用程序
-		// 进行显示
-		wordCounts.foreachRDD(new Function<JavaPairRDD<String,Integer>, Void>() {
+                    private static final long serialVersionUID = 1L;
 
-			private static final long serialVersionUID = 1L;
+                    @Override
+                    public Tuple2<String, Integer> call(String word)
+                            throws Exception {
+                        return new Tuple2<String, Integer>(word, 1);
+                    }
 
-			@Override
-			public Void call(JavaPairRDD<String, Integer> wordCountsRDD) throws Exception {
-				// 调用RDD的foreachPartition()方法
-				wordCountsRDD.foreachPartition(new VoidFunction<Iterator<Tuple2<String,Integer>>>() {
+                });
 
-					private static final long serialVersionUID = 1L;
+        JavaPairDStream<String, Integer> wordCounts = pairs.updateStateByKey(
 
-					@Override
-					public void call(Iterator<Tuple2<String, Integer>> wordCounts) throws Exception {
-						// 给每个partition，获取一个连接
-						Connection conn = ConnectionPool.getConnection();
-					
-						// 遍历partition中的数据，使用一个连接，插入数据库
-						Tuple2<String, Integer> wordCount = null;
-						while(wordCounts.hasNext()) {
-							wordCount = wordCounts.next();
-							
-							String sql = "insert into wordcount(word,count) "
-									+ "values('" + wordCount._1 + "'," + wordCount._2 + ")";  
-							
-							Statement stmt = conn.createStatement();
-							stmt.executeUpdate(sql);
-						}
-						
-						// 用完以后，将连接还回去
-						ConnectionPool.returnConnection(conn);
-					}
-				});
-				
-				return null;
-			}
-			
-		});
-		
-		jssc.start();
-		jssc.awaitTermination();
-		jssc.close();
-	}
-	
+                new Function2<List<Integer>, Optional<Integer>, Optional<Integer>>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Optional<Integer> call(List<Integer> values,
+                                                  Optional<Integer> state) throws Exception {
+                        Integer newValue = 0;
+
+                        if (state.isPresent()) {
+                            newValue = state.get();
+                        }
+
+                        for (Integer value : values) {
+                            newValue += value;
+                        }
+
+                        return Optional.of(newValue);
+                    }
+
+                });
+
+        // 每次得到当前所有单词的统计次数之后，将其写入mysql存储，进行持久化，以便于后续的J2EE应用程序
+        // 进行显示
+        wordCounts.foreachRDD(new Function<JavaPairRDD<String, Integer>, Void>() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Void call(JavaPairRDD<String, Integer> wordCountsRDD) throws Exception {
+                // 调用RDD的foreachPartition()方法
+                wordCountsRDD.foreachPartition(new VoidFunction<Iterator<Tuple2<String, Integer>>>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public void call(Iterator<Tuple2<String, Integer>> wordCounts) throws Exception {
+                        // 给每个partition，获取一个连接
+                        Connection conn = ConnectionPool.getConnection();
+
+                        // 遍历partition中的数据，使用一个连接，插入数据库
+                        Tuple2<String, Integer> wordCount = null;
+                        while (wordCounts.hasNext()) {
+                            wordCount = wordCounts.next();
+
+                            String sql = "insert into wordcount(word,count) "
+                                    + "values('" + wordCount._1 + "'," + wordCount._2 + ")";
+
+                            Statement stmt = conn.createStatement();
+                            stmt.executeUpdate(sql);
+                        }
+
+                        // 用完以后，将连接还回去
+                        ConnectionPool.returnConnection(conn);
+                    }
+                });
+
+                return null;
+            }
+
+        });
+
+        jssc.start();
+        jssc.awaitTermination();
+        jssc.close();
+    }
+
 }
